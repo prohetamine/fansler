bb7f5f4a2f33e2b752d350ee7646378e6c2c3e9e
Stas Prohetamine <prohetamine@gmail.com>
2023-03-21 16:50:48 +0300

    update

--- src/parse-username-0.js
+++ src/parse-username-0.js
@@ -4,7 +4,8 @@ const request = require('request-promise')
     , readline = require('readline')
     , sleep = require('sleep-promise')
     , proxy = require('./libs/proxy')
-    , { fork } = require('child_process')
+    , fetch = require('node-fetch')
+    , HttpsProxyAgent = require('https-proxy-agent')
     , syncRandom = array =>
         array.map(elem => [elem, Math.random()]).sort((a, b) => a[1] - b[1]).map(elem => elem[0])
 
@@ -62,22 +63,37 @@ const apiRequest = async proxy => {
   if (usernames.length > 0) {
     const _usernames = usernames.shift()
     try {
-      const result = await new Promise(res => {
-        const _request = fork(__dirname + '/libs/request.js', [proxy, _usernames])
-        _request.on("message", message => {
-          res(`${message}`.trim())
-        })
+      const result = await fetch(`https://apiv3.fansly.com/api/v1/account?usernames=${_usernames}&ngsw-bypass=true`, {
+        agent: new HttpsProxyAgent(`http://${proxy}`),
+        headers: {
+          "authority": "apiv3.fansly.com",
+          "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+          "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+          "sec-ch-ua": '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"macOS"',
+          "sec-fetch-dest": "document",
+          "sec-fetch-mode": "navigate",
+          "sec-fetch-site": "none",
+          "sec-fetch-user": "?1",
+          "upgrade-insecure-requests": "1",
+          "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+        }
       })
+      .then(e => e.text())
 
-      if (result === 'timeout') {
-        throw new Error(result)
+      const data = JSON.parse(result)
+
+      if (data.success) {
+        accounts = data.response.filter(
+          f =>
+            f.followCount < 10 &&
+            f.postLikes === 0 &&
+            f.accountMediaLikes === 0 &&
+            !f.subscriptionTiers &&
+            !f.pinnedPosts
+        ).map(e => e.username)
       }
-
-      if (result === 'success bad') {
-        throw new Error(result)
-      }
-
-      accounts = JSON.parse(result)
     } catch (e) {
       usernames.unshift(_usernames)
     }
@@ -133,7 +149,7 @@ const run = async proxy => {
     const proxyLength = proxy().length > 5
 
     if (proxyLength) {
-      proxy().slice(0, 200).map(run)
+      proxy().slice(0, 10).map(run)
     } else {
       console.log('wait proxy')
     }
