e4f4d83e4831aae468a169e9e986613fd81eb398
Stas Prohetamine <prohetamine@gmail.com>
2023-03-22 12:23:30 +0300

    update

--- src/libs/db.js
+++ src/libs/db.js
@@ -2,7 +2,7 @@ const fs = require('fs')
     , path = require('path')
 
 module.exports = class {
-  constructor (name) {
+  constructor (name, timeout = 60000) {
     this.basePath = path.join(__dirname, '')
     this.baseTmpPath = path.join(this.basePath, `tmp-${name}.json`)
     this.basePerPath = path.join(this.basePath, `${name}.json`)
@@ -10,39 +10,38 @@ module.exports = class {
     if (!fs.existsSync(this.basePerPath)) {
       fs.writeFileSync(this.basePerPath, JSON.stringify({}))
     }
+
+    this.db = JSON.parse(fs.readFileSync(this.basePerPath, 'utf8'))
+
+    setInterval(() => {
+      fs.writeFileSync(this.baseTmpPath, JSON.stringify(this.db))
+      for (;;) {
+        try {
+          fs.renameSync(this.baseTmpPath, this.basePerPath)
+          break
+        } catch (e) {
+          console.log(`Err save: ${this.baseTmpPath}`)
+        }
+      }
+    }, timeout)
   }
 
   check (key, data) {
-    const db = JSON.parse(fs.readFileSync(this.basePerPath, 'utf8'))
-    return !!db[key][data]
+    return !!this.db[key][data]
   }
 
   set (key, meta) {
-    const db = JSON.parse(fs.readFileSync(this.basePerPath, 'utf8'))
-
     try {
-      db[key] = {
-        ...db[key],
+      this.db[key] = {
+        ...this.db[key],
         ...meta
       }
     } catch (e) {
-      db[key] = meta
-    }
-
-
-    fs.writeFileSync(this.baseTmpPath, JSON.stringify(db))
-    for (;;) {
-      try {
-        fs.renameSync(this.baseTmpPath, this.basePerPath)
-        break
-      } catch (e) {
-        console.log(`Err save: ${this.baseTmpPath}`)
-      }
+      this.db[key] = meta
     }
   }
-  
+
   get () {
-    const db = JSON.parse(fs.readFileSync(this.basePerPath, 'utf8'))
-    return Object.keys(db).map(key => ({ name: key, ...db[key] }))
+    return Object.keys(this.db).map(key => ({ name: key, ...this.db[key] }))
   }
 }
